a22b352a356ceaac1cb2a1c8b7541186436c9161
Екатерина <pepperlug@yandex.ru>
2025-08-17 22:16:38 +0700

    рефакторинг: оформила вспомогательные методы для всего теста, вынесла логин-пароль для авторизации и данные о группе в параметры методов

--- test_add_group.py
+++ test_add_group.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.firefox.options import Options
-import unittest, time, re
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -17,29 +14,60 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd, user="admin", password="secret")
+        self.open_groups(wd)
+        self.add_new_group(wd, "stone", "stone", "stone")
+        self.submit_new_group(wd)
+        self.return_to_groups(wd)
+        self.logout(wd)
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, user="", password="")
+        self.open_groups(wd)
+        self.add_new_group(wd, name="stone", header="stone", footer="stone")
+        self.submit_new_group(wd)
+        self.return_to_groups(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def submit_new_group(self, wd):
+        wd.find_element_by_name("submit").click()
+
+    def add_new_group(self, wd, name, header, footer):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("stone")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("stone")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("stone")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+        wd.find_element_by_name("group_footer").send_keys(footer)
+
+    def open_groups(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd, user, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(user)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
