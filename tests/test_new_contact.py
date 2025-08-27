76eac3f7ddc72a9583b929d759756c138bc311dd
Екатерина <pepperlug@yandex.ru>
2025-08-27 07:36:45 +0700

    добавила новый тест на удаление первой группы в списке, реализовала общую фикстуру для всех тестов

--- tests/test_new_contact.py
+++ tests/test_new_contact.py
@@ -4,6 +4,6 @@ from model.features_contact import FeaturesContact
 def test_new_contact(app):
     app.session.login(user="admin", password="secret")
     app.contact.form_new_contact()
-    app.contact.add_new_contact(FeaturesContact(firstname="John", middlename="Snow", lastname="Aegon", nickname="Targaryen",title="north", company="night watch", address="black castle",home="111", mobile="222", work="333",fax="dark dozor", email="targaryen@gmail.com", bday="4",bmonth="May",byear="283",new_group = "jojo"))
+    app.contact.add_new_contact(FeaturesContact(firstname="John", middlename="Snow", lastname="Aegon", nickname="Targaryen",title="north", company="night watch", address="black castle",home="111", mobile="222", work="333",fax="dark dozor", email="targaryen@gmail.com", bday="4",bmonth="May",byear="283",new_group = "stone"))
     app.session.logout()
 
