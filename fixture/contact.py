26ddca700a5c7afec2ff28919fa91d720ea09bde
Екатерина <pepperlug@yandex.ru>
2025-08-31 18:07:46 +0700

    реализован тест на редактирование первого контакта+ вспомогательный метод для редактирования контакта соответственно

--- fixture/contact.py
+++ fixture/contact.py
@@ -54,10 +54,68 @@ class ContactHelper:
         Select(wd.find_element_by_name("new_group")).select_by_visible_text(features_contact.new_group)
         wd.find_element_by_xpath("//select[@name='new_group']/option[text()='" + features_contact.new_group + "']").click()
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+
     def form_new_contact(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+
+    def update_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
     def del_first_contact(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
+
+    def edit_first_contact(self,features_contact,new_group=None):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(features_contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(features_contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(features_contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(features_contact.nickname)
+        # вводим заголовок, компанию, адрес нового контакта
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(features_contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(features_contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(features_contact.address)
+
+        # вводим домашний, рабочий, мобильный телефоны нового контакта
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(features_contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(features_contact.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(features_contact.work)
+        # вводим доп.способы связи: факс и электронную почту
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(features_contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(features_contact.email)
+        if new_group is not None:
+            # Выбираем группу для нового контакта
+            wd.find_element_by_name("new_group").click()
+            Select(wd.find_element_by_name("new_group")).select_by_visible_text(features_contact.new_group)
+            wd.find_element_by_xpath(
+            "//select[@name='new_group']/option[text()='" + features_contact.new_group + "']").click()
+            wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+        self.update_contact()
---
+++ tests/test_edit_contact.py
@@ -0,0 +1,6 @@
+from model.features_contact import FeaturesContact
+
+def test_edit_first_contact(app):
+    app.session.login(user="admin", password="secret")
+    app.contact.edit_first_contact(FeaturesContact(firstname="Davos", middlename="Sivort", lastname="Luk", nickname="Desni", title="dragon stone",company="stannys", address="black castle", home="2234", mobile="2224", work="3353",fax="dark dozor1", email="targaryen@gmail.com", bday="4", bmonth="May", byear="283",new_group="sand"))
+    app.session.logout()
